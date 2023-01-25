c00f6b380098bcdb99f2afb5e4303ad4c07aa0bf
abSimone <s.angelonibei@digitality-consulting.com>
2023-01-25 16:47:03 +0100

    fix modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,21 @@ import { AddCustomerComponent } from './add-customer/add-customer.component';
 import { DetailsCustomerComponent } from './details-customer/details-customer.component';
 import { HomeComponent } from './home/home.component';
 import { ListaCustomerComponent } from './lista-customer/lista-customer.component';
+import { TabellaComponent } from './components/tabella/tabella.component';
+import { FormComponent } from './components/form/form.component';
+import { DettaglioComponent } from './components/dettaglio/dettaglio.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, children: [] },
-  { path: 'lista', component: ListaEmployeeComponentComponent},
+  { path: 'lista', component: ListaEmployeeComponentComponent },
   { path: 'aggiungi', component: AddEmployeeComponent },
-  {path: 'listCustomer', component: ListaCustomerComponent},
-  {path: 'detailsCustomer', component: DetailsCustomerComponent },
-  {path: 'addCustomer', component: AddCustomerComponent},
-  { path: '**', redirectTo: '' }
-
+  { path: 'listCustomer', component: ListaCustomerComponent },
+  { path: 'detailsCustomer', component: DetailsCustomerComponent },
+  { path: 'addCustomer', component: AddCustomerComponent },
+  { path: 'tabella', component: TabellaComponent },
+  { path: 'form', component: FormComponent },
+  { path: 'dettaglio', component: DettaglioComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,19 @@
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormComponent } from './components/form/form.component';
 import { TabellaComponent } from './components/tabella/tabella.component';
 import { DettaglioComponent } from './components/dettaglio/dettaglio.component';
-import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListaCustomerComponent } from './lista-customer/lista-customer.component';
 import { AddCustomerComponent } from './add-customer/add-customer.component';
 import { DetailsCustomerComponent } from './details-customer/details-customer.component';
 import { ListaEmployeeComponentComponent } from './lista-employee-component/lista-employee-component.component';
-
-const routes: Routes = [
-
-  {path: "", component: HomeComponent},
-  {path: "tabella", component: TabellaComponent},
-  {path: "form", component: FormComponent},
-  {path: "dettaglio", component: DettaglioComponent}
-
-];
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -37,17 +27,11 @@ const routes: Routes = [
     AddCustomerComponent,
     DetailsCustomerComponent,
     ListaEmployeeComponentComponent,
-    AddEmployeeComponent
+    AddEmployeeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule],
 
   providers: [],
   bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
